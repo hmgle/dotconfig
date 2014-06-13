b8773026b06b1e3d46698f33b70d47e509a28c1e
mingang.he <dustgle@gmail.com>
2014-06-13 10:08:14 +0800

    xfce4 config

--- install.sh
+++ install.sh
@@ -9,3 +9,10 @@ fi
 if [[ ! -e ~/.git_template ]]; then
 	ln -sf ${PWD}/git_template ~/.git_template
 fi
+
+
+
+if [[ -e ~/.config/xfce4 ]]; then
+	mv ~/.config/xfce4{,.bak}
+fi
+ln -sf ${PWD}/config/xfce4 ~/.config/xfce4
