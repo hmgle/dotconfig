4b2f7b185d04aab3e353918e844a1006cf790fc8
mingang.he <dustgle@gmail.com>
2014-06-14 10:41:49 +0800

    oh-my-zsh

--- install.sh
+++ install.sh
@@ -16,3 +16,10 @@ if [[ -e ~/.config/xfce4 ]]; then
 	mv ~/.config/xfce4{,.bak}
 fi
 ln -sf ${PWD}/config/xfce4 ~/.config/xfce4
+
+# install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
+if [[ -e ~/.zshrc ]]; then
+	mv ~/.zshrc{,.bak}
+fi
+ln -sf ${PWD}/zshrc ~/.zshrc
