248a0f44a992fac3f25d0720baa2b503087ca96e
mingang.he <dustgle@gmail.com>
2015-06-15 12:26:39 +0800

    fix udev rules shell

--- bin/pokerkeyboard_in.sh
+++ bin/pokerkeyboard_in.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-/usr/bin/xmodmap /home/gle/.pokerXmodmap
+/bin/sleep 0.2 && sudo -u gle /usr/bin/xmodmap -display :0 /home/gle/.pokerXmodmap >/dev/null 2>&1 &
+# /bin/sleep 1 && /usr/bin/xmodmap /home/gle/code_repo/misc/dotconfig/pokerXmodmap >/dev/null 2>&1 & # no work
+# /bin/sleep 0.2 && /usr/bin/xmodmap -display :0 /home/gle/code_repo/misc/dotconfig/pokerXmodmap >/dev/null 2>&1 & # not work
+# /bin/sleep 0.2 && sudo -u gle /usr/bin/xmodmap /home/gle/code_repo/misc/dotconfig/pokerXmodmap >/dev/null 2>&1 & # not work
--- bin/pokerkeyboard_out.sh
+++ bin/pokerkeyboard_out.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-/usr/bin/xmodmap /home/gle/.Xmodmap
+/bin/sleep 0.2
+if [ ! -e /dev/input/by-id/usb-Heng_Yu_Technology_Poker_II-event-kbd ]; then
+    sudo -u gle /usr/bin/xmodmap -display :0 /home/gle/.Xmodmap >/dev/null 2>&1 &
+fi
