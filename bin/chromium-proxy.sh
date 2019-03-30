a28cc93fccb7e45e9352ac6b07d0409dfcb3bb6f
mingang.he <dustgle@gmail.com>
2019-03-30 19:01:29 +0800

    Update: awesome wm 3.4=>3.5

--- bin/chromium-proxy.sh
+++ bin/chromium-proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-chromium-browser -proxy-pac-url=file:///home/gle/data/pri/shadowsocks_etc/socks5.pac
+# chromium-browser -proxy-pac-url=file:///home/gle/data/pri/shadowsocks_etc/socks5.pac
+chromium-browser -proxy-pac-url=file:///home/gle/data/pri/socks5-config/socks5.pac
