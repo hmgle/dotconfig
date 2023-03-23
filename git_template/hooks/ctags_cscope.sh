b6d5d7381dfa2f3d6d31d359a047f3cc84040639
mingang.he <dustgle@gmail.com>
2023-03-23 16:39:38 +0800

    Update

--- git_template/hooks/ctags_cscope.sh
+++ git_template/hooks/ctags_cscope.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cfiles=`find -maxdepth 2 -name "*.[ch]"`
-if [ ${#cfiles[@]} -gt 0 ]; then 
-	ctags -R
-	cscope -Rbq
-fi
-
+## ctags/cscope was deprecated, switch to LSP.
+# cfiles=`find -maxdepth 2 -name "*.[ch]"`
+# if [ ${#cfiles[@]} -gt 0 ]; then
+# 	ctags -R
+# 	cscope -Rbq
+# fi
