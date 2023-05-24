4f6de3c4ce7471c04d61682f283602e28b5a6f44
mingang.he <dustgle@gmail.com>
2023-05-24 15:34:29 +0800

    Update xmodmap

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ PWD=$(pwd)
 if [[ -e ~/.gitconfig ]]; then
 	mv ~/.gitconfig{,.bak}
 fi
-ln -sf ${PWD}/.gitconfig ~/.gitconfig
+ln -sf "${PWD}"/.gitconfig ~/.gitconfig
 
 if [[ -e ~/.git_template ]]; then
 	mv ~/.git_template{,.bak}
 fi
-ln -sf ${PWD}/git_template ~/.git_template
+ln -sf "${PWD}"/git_template ~/.git_template
 
 # if [[ -e ~/.config/xfce4 ]]; then
 # 	mv ~/.config/xfce4{,.bak}
@@ -25,42 +25,45 @@ fi
 if [[ -e ~/.zshrc ]]; then
 	mv ~/.zshrc{,.bak}
 fi
-ln -sf ${PWD}/zshrc ~/.zshrc
+ln -sf "${PWD}"/zshrc ~/.zshrc
 
 if [[ -e ~/.zshenv ]]; then
 	mv ~/.zshenv{,.bak}
 fi
-ln -sf ${PWD}/zshenv ~/.zshenv
+ln -sf "${PWD}"/zshenv ~/.zshenv
 
 # bashrc
 if [[ ! -e ~/.bashrc ]]; then
 	mv ~/.bashrc{,.bak}
 fi
-ln -sf ${PWD}/bashrc ~/.bashrc
+ln -sf "${PWD}"/bashrc ~/.bashrc
 
 # tmux
 if [[ -e ~/.tmux.conf ]]; then
 	mv ~/.tmux.conf{,.bak}
 fi
-ln -sf ${PWD}/tmux.conf ~/.tmux.conf
+ln -sf "${PWD}"/tmux.conf ~/.tmux.conf
 
 # tool
 if [[ ! -e ~/bin ]]; then
-	ln -sf ${PWD}/bin ~/bin
+	ln -sf "${PWD}"/bin ~/bin
 fi
 
 # xmodmap
-if [[ ! -e ~/.Xmodmap ]]; then
-	ln -sf ${PWD}/tpXmodmap ~/.tpXmodmap
+if [[ ! -e ~/.tpXmodmap ]]; then
+	ln -sf "${PWD}"/tpXmodmap ~/.tpXmodmap
 fi
 if [[ ! -e ~/.pokerXmodmap ]]; then
-	ln -sf ${PWD}/pokerXmodmap ~/.pokerXmodmap
+	ln -sf "${PWD}"/pokerXmodmap ~/.pokerXmodmap
+fi
+if [[ ! -e ~/.Xmodmap ]]; then
+	ln -sf "${PWD}"/k6Xmodmap ~/.Xmodmap
 fi
 if [[ ! -e ~/.xinitrc ]]; then
-	ln -sf ${PWD}/xinitrc ~/.xinitrc
+	ln -sf "${PWD}"/xinitrc ~/.xinitrc
 fi
 if [[ ! -e /etc/udev/rules.d/80-keyboard.rules ]]; then
-	sudo cp ${PWD}/80-keyboard.rules /etc/udev/rules.d/80-keyboard.rules
+	sudo cp "${PWD}"/80-keyboard.rules /etc/udev/rules.d/80-keyboard.rules
 	sudo udevadm control --reload-rules
 fi
 
@@ -69,13 +72,13 @@ if [[ -e ~/.config/awesome/rc.lua ]]; then
 	mv ~/.config/awesome/rc.lua{,.bak}
 fi
 mkdir -p ~/.config/awesome
-ln -sf ${PWD}/config/awesome/rc.lua ~/.config/awesome/rc.lua
+ln -sf "${PWD}"/config/awesome/rc.lua ~/.config/awesome/rc.lua
 
 # spacemacs
 if [[ -e ~/.spacemacs ]]; then
 	mv ~/.spacemacs{,.bak}
 fi
-ln -sf ${PWD}/spacemacs-conf/spacemacs.symlink ~/.spacemacs
+ln -sf "${PWD}"/spacemacs-conf/spacemacs.symlink ~/.spacemacs
 
 # tpm
 if [[ ! -e ~/.tmux/plugins ]]; then
